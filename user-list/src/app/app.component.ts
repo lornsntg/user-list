33f89b7b85a1e57026e67fa203cbdc01dbfb9713
lornsntg bho <buhaj.lorenzo@iisgalvanimi.edu.it>
2025-03-31 11:58:17 +0000

    meta lavoro svolto

--- user-list/src/app/app.component.ts
+++ user-list/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloWorldComponent, UserItemComponent],
+  imports: [RouterOutlet, HelloWorldComponent, UserItemComponent, UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
