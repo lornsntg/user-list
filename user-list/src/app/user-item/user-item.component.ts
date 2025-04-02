efe21795b1f2124b87191d44e8fd73964582dbe5
lornsntg bho <buhaj.lorenzo@iisgalvanimi.edu.it>
2025-04-02 12:32:57 +0000

    using array svolto

--- user-list/src/app/user-item/user-item.component.ts
+++ user-list/src/app/user-item/user-item.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import {Component,OnInit,Input} from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
-  imports: [],
   templateUrl: './user-item.component.html',
-  styleUrl: './user-item.component.css'
+  styleUrls: ['./user-item.component.css']
 })
+
 export class UserItemComponent implements OnInit {
-  name: string; // <-- aggiunta la proprietà name
+  @Input() name!: string; 
   constructor() { 
-    this.name = 'Felipe'; // setta il nome nel costruttore
+
   }
   ngOnInit() {
   }
